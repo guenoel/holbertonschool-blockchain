22b19ed0e2daacd852c85e61b481043f14b6ec28
guenoel <guenooel@hotmail.com>
2024-02-29 02:32:47 -0800

    task 5 check fp

--- crypto/ec_load.c
+++ crypto/ec_load.c
@@ -1,60 +1,43 @@
 #include "hblk_crypto.h"
 
 /**
- * load_ec_key - loads an EC key pair from the disk
+ * ec_load - loads an EC key pair from the disk
  *
- * @key: pointer to the EC key pair to be loaded
  * @folder: path to the folder in which to save the keys
- * @filename: name of the file in which to save the key
  *
- * Return: If key, folder, or filename is NULL, return 0.
- * Otherwise, return 1.
+ * Return: If folder is NULL, return NULL.
+ * Otherwise, return a pointer to the created EC_KEY structure.
  */
-EC_KEY *load_ec_key(EC_KEY *key, char const *folder, char const *filename)
+EC_KEY *ec_load(char const *folder)
 {
 	char path[1024];
 	FILE *fp;
-
-	sprintf(path, "%s/%s", folder, filename);
-	fp = fopen(path, "r");
-	if (!fp)
-		return (NULL);
-	fclose(fp);
-
-	if (strcmp(filename, PUB_FILENAME) == 0)
-	{
-		if (!PEM_read_EC_PUBKEY(fp, &key, NULL, NULL))
-		{
-			EC_KEY_free(key);
-			fclose(fp);
-			return (NULL);
-		}
-		fclose(fp);
-	}
-	if (strcmp(filename, PRI_FILENAME) == 0)
-	{
-		if (!PEM_read_ECPrivateKey(fp, &key, NULL, NULL))
-		{
-			EC_KEY_free(key);
-			fclose(fp);
-			return (0);
-		}
-		fclose(fp);
-	}
-
-	return (key);
-}
-
-EC_KEY *ec_load(char const *folder)
-{
 	EC_KEY *key;
 
 	if (!folder)
 		return (NULL);
 
-	key = EC_KEY_new_by_curve_name(EC_CURVE);
-	if (!key)
+	sprintf(path, "%s/%s", folder, PUB_FILENAME);
+	fp = fopen(path, "r");
+	if (!fp)
 		return (NULL);
+	if (!PEM_read_EC_PUBKEY(fp, &key, NULL, NULL))
+	{
+		EC_KEY_free(key);
+		fclose(fp);
+		return (NULL);
+	}
+	sprintf(path, "%s/%s", folder, PRI_FILENAME);
+	fp = fopen(path, "r");
+	if (!fp)
+		return (NULL);
+	if (!PEM_read_ECPrivateKey(fp, &key, NULL, NULL))
+	{
+		EC_KEY_free(key);
+		fclose(fp);
+		return (NULL);
+	}
+	fclose(fp);
 
-	return (load_ec_key(key, folder, PUB_FILENAME));
+	return (key);
 }
