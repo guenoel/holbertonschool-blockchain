5a23b8d7be43126c07f37914021c0550747a2d0f
guenoel <guenooel@hotmail.com>
2024-02-29 06:57:55 -0800

    task 7 ec_verify

---
+++ crypto/ec_verify.c
@@ -0,0 +1,21 @@
+#include "hblk_crypto.h"
+
+/**
+ * ec_verify - verifies the signature of a given set of bytes, using a given
+ * EC_KEY public key
+ *
+ * @key: pointer to the EC_KEY structure containing the public key to be used
+ * @msg: characters to be verified
+ * @msglen: number of characters to be verified
+ * @sig: signature to be checked
+ *
+ * Return: If key, msg, or sig is NULL, or upon failure, return 0.
+ * Otherwise, return 1 if the signature is valid, and 0 if it is not.
+ */
+int ec_verify(EC_KEY const *key, uint8_t const *msg, size_t msglen,
+				sig_t const *sig)
+{
+	if (!key || !msg || !msglen || !sig)
+		return (0);
+	return (ECDSA_verify(0, msg, msglen, sig->sig, sig->len, (EC_KEY *)key));
+}
