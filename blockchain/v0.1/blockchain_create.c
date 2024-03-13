f1567f845a4e400e08557c03825764033ae32f01
guenoel <guenooel@hotmail.com>
2024-03-13 02:38:17 -0700

    task 0

---
+++ blockchain/v0.1/blockchain_create.c
@@ -0,0 +1,47 @@
+#include "blockchain.h"
+
+/**
+ * blockchain_create - creates a Blockchain structure, and initializes it
+ *
+ * Return: a pointer to the created blockchain, or NULL on failure
+ */
+blockchain_t *blockchain_create(void)
+{
+	blockchain_t *blockchain = calloc(1, sizeof(*blockchain));
+	block_t *genesis = calloc(1, sizeof(*genesis));
+	llist_t *list = NULL;
+
+	if (!blockchain || !genesis)
+	{
+		free(blockchain);
+		free(genesis);
+		return (NULL);
+	}
+
+	list = llist_create(MT_SUPPORT_TRUE);
+	if (!list)
+	{
+		free(blockchain);
+		free(genesis);
+		return (NULL);
+	}
+
+	genesis->info.index = 0;
+	genesis->info.difficulty = 0;
+	genesis->info.timestamp = 1537578000;
+	genesis->info.nonce = 0;
+	memset(genesis->info.prev_hash, 0, SHA256_DIGEST_LENGTH);
+	memset(genesis->data.buffer, 0, BLOCKCHAIN_DATA_MAX);
+
+	if (llist_add_node(list, genesis, ADD_NODE_FRONT) == -1)
+	{
+		free(blockchain);
+		free(genesis);
+		llist_destroy(list, 1, NULL);
+		return (NULL);
+	}
+
+	blockchain->chain = list;
+
+	return (blockchain);
+}
