e47d58a59648f52c4731e4971241615e9c6f74a9
guenoel <guenooel@hotmail.com>
2024-03-25 08:16:27 -0700

    change in sha256b function

--- blockchain/v0.3/transaction/tx_out_create.c
+++ blockchain/v0.3/transaction/tx_out_create.c
@@ -18,7 +18,7 @@ tx_out_t *tx_out_create(uint32_t amount, uint8_t const pub[EC_PUB_LEN])
 		return (NULL);
 	tx_out->amount = amount;
 	memcpy(tx_out->pub, pub, sizeof(tx_out->pub));
-	if (!sha256((int8_t const *)&tx_out->amount,
+	if (!sha256((int8_t const *)tx_out,
 		sizeof(tx_out->amount) + sizeof(tx_out->pub), tx_out->hash))
 	{
 		free(tx_out);
