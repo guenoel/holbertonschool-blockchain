db1f2c9ed9bdff3627268c542396f08334ac2203
guenoel <guenooel@hotmail.com>
2024-04-03 07:57:42 -0700

    back to previous code

--- blockchain/v0.3/block_create.c
+++ blockchain/v0.3/block_create.c
@@ -9,34 +9,33 @@
 * Return: A pointer to the allocated Block
 */
 block_t *block_create(block_t const *prev, int8_t const *data,
-															uint32_t data_len)
+						uint32_t data_len)
 {
-	block_t *block; /* Block to create */
-	/* Get the minimum of data_len and BLOCKCHAIN_DATA_MAX */
+	block_t *block;
 	uint32_t max_len = data_len > BLOCKCHAIN_DATA_MAX ?
 		BLOCKCHAIN_DATA_MAX : data_len;
-	/* List of transactions */
-	llist_t *transactions = llist_create(MT_SUPPORT_FALSE);
+	llist_t *list_tx;
 
-	/* Check if the previous block and data pointers are NULL */
 	if (!prev || !data)
 		return (NULL);
-
-	/* Allocate memory for the new block */
 	block = calloc(1, sizeof(*block));
+	if (!block)
+		return (NULL);
 
-	if (!block || !transactions)
-		return (free(block), llist_destroy(transactions, 0, NULL), NULL);
-
-	/* Set the block's info */
 	memcpy(block->data.buffer, data, max_len);
 	block->data.len = max_len;
 
-	/* Set the block's info */
 	memcpy(block->info.prev_hash, prev->hash, SHA256_DIGEST_LENGTH);
 	block->info.index = prev->info.index + 1;
 	block->info.timestamp = (uint64_t)time(NULL);
-	block->transactions = transactions;
+
+	list_tx = llist_create(MT_SUPPORT_FALSE);
+	if (!list_tx)
+	{
+		free(block);
+		return (NULL);
+	}
+	block->transactions = list_tx;
 
 	return (block);
 }
