d33831cb4621b32a41b547c168525942f7988502
guenoel <guenooel@hotmail.com>
2024-04-03 07:56:03 -0700

    add destroy

--- blockchain/v0.3/block_destroy.c
+++ blockchain/v0.3/block_destroy.c
@@ -6,6 +6,11 @@
 */
 void block_destroy(block_t *block)
 {
+	if (!block)
+		return;
 	if (block)
+	{
+		llist_destroy(block->transactions, 1, (node_dtor_t)transaction_destroy);
 		free(block);
+	}
 }
