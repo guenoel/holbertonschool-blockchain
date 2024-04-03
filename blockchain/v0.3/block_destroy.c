5473aa2264bf1e54c5caa499da24fcfb84a265de
guenoel <guenooel@hotmail.com>
2024-04-03 08:05:15 -0700

    remove a condition

--- blockchain/v0.3/block_destroy.c
+++ blockchain/v0.3/block_destroy.c
@@ -8,9 +8,6 @@ void block_destroy(block_t *block)
 {
 	if (!block)
 		return;
-	if (block)
-	{
-		llist_destroy(block->transactions, 1, (node_dtor_t)transaction_destroy);
-		free(block);
-	}
+	llist_destroy(block->transactions, 1, (node_dtor_t)transaction_destroy);
+	free(block);
 }
